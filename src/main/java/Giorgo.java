c874777bdf4e9efd3c9edc702ecf30a36587f7b8
Ewen <e1113211@u.nus.edu>
2024-08-19 13:56:37 +0800

    Level-2

--- src/main/java/Giorgo.java
+++ src/main/java/Giorgo.java
@@ -4,6 +4,8 @@ public class Giorgo {
     public static void main(String[] args) {
         String logo = " Giorgo ";
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100]; // Fixed-size array to store tasks
+        int taskCount = 0; // Keep track of the number of tasks
 
         System.out.println(
                 "____________________________________________________________\n" +
@@ -15,11 +17,31 @@ public class Giorgo {
         do {
             input = scanner.nextLine();
 
-            if (!input.equalsIgnoreCase("bye")) {
-                System.out.println(
-                        "____________________________________________________________\n" +
-                                " " + input + "\n" +
-                                "____________________________________________________________\n");
+            if (input.equalsIgnoreCase("list")) {
+                // List all stored tasks
+                if (taskCount == 0) {
+                    System.out.println("____________________________________________________________\n" +
+                            " No tasks yet.\n" +
+                            "____________________________________________________________\n");
+                } else {
+                    System.out.println("____________________________________________________________\n");
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println((i + 1) + ". " + tasks[i]);
+                    }
+                    System.out.println("____________________________________________________________\n");
+                }
+            } else if (!input.equalsIgnoreCase("bye")) {
+                // Add task if it's not "bye" and there's space
+                if (taskCount < 100) {
+                    tasks[taskCount++] = input;
+                    System.out.println("____________________________________________________________\n" +
+                            " added: " + input + "\n" +
+                            "____________________________________________________________\n");
+                } else {
+                    System.out.println("____________________________________________________________\n" +
+                            " Task list is full. Cannot add more tasks.\n" +
+                            "____________________________________________________________\n");
+                }
             }
 
         } while (!input.equalsIgnoreCase("bye"));
