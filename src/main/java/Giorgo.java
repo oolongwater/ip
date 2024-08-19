5bcce828773bb792e4431726efad945b95a40278
Ewen <e1113211@u.nus.edu>
2024-08-19 13:54:26 +0800

    Level-1

--- src/main/java/Giorgo.java
+++ src/main/java/Giorgo.java
@@ -1,11 +1,34 @@
+import java.util.Scanner;
+
 public class Giorgo {
     public static void main(String[] args) {
         String logo = " Giorgo ";
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println(
-                " Hello! I'm " + logo + "\n" + // Print
-                " What can I do for you?\n" +
                 "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n");
+                        " Hello! I'm " + logo + "\n" +
+                        " What can I do for you?\n" +
+                        "____________________________________________________________\n");
+
+        String input;
+        do {
+            input = scanner.nextLine();
+
+            if (!input.equalsIgnoreCase("bye")) {
+                System.out.println(
+                        "____________________________________________________________\n" +
+                                " " + input + "\n" +
+                                "____________________________________________________________\n");
+            }
+
+        } while (!input.equalsIgnoreCase("bye"));
+
+        System.out.println(
+                "____________________________________________________________\n" +
+                        " Bye. Hope to see you again soon!\n" +
+                        "____________________________________________________________\n");
+
+        scanner.close();
     }
 }
