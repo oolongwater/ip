96bac239fa28d6637e36e69f14925d07f15dfce2
Ewen <e1113211@u.nus.edu>
2024-08-19 13:45:11 +0800

    Level-0

---
+++ src/main/java/Giorgo.java
@@ -0,0 +1,11 @@
+public class Giorgo {
+    public static void main(String[] args) {
+        String logo = " Giorgo ";
+        System.out.println(
+                " Hello! I'm " + logo + "\n" + // Print
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n");
+    }
+}
