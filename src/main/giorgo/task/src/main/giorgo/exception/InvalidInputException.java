5bfd29a480c7a084c634ca8167246276c44bbc2d
Ewen <e1113211@u.nus.edu>
2024-08-21 13:12:03 +0800

    Deleted extra src

--- src/main/giorgo/task/src/main/giorgo/exception/InvalidInputException.java
+++
@@ -1,8 +0,0 @@
-package task.src.main.giorgo.exception;
-
-public class InvalidInputException extends Exception {
-    public InvalidInputException(String message) {
-        super(message);
-        System.out.println("ERROR");
-    }
-}
